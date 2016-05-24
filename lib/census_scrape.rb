80eb32169c2a4552f385c7a1b6a68c6352afe70f
Michael Weigle <michael.weigle@gmail.com>
2016-05-24 13:06:48 -0400

    should go clean this up

--- lib/census_scrape.rb
+++ lib/census_scrape.rb
@@ -17,152 +17,169 @@ class Scraper
   end
 
   def initialize
-    @csv = CSV.open("acs5.csv", "w+", headers: true, force_quotes: true)
-    @csv << %w(state cd sldu sldl county tract bg field_id label value)
+    @csv = CSV.open("acs5-#{Time.now.to_i}.csv", "w+", headers: true, force_quotes: true)
+    @csv << %w(loc_id state cd sldu sldl county tract bg field_id value)
     setup_acs_vars
   end
 
+  def reset_csv(suffix=Time.now.to_i)
+    @csv.close
+    @csv = CSV.open("acs5-#{suffix}.csv", "w+", headers: true, force_quotes: true)
+    @csv << %w(loc_id state cd sldu sldl county tract bg field_id value)
+  end
+
+  def state_ids
+    @state_ids ||= @@client.where({fields: @@acs_vars.keys.first, level: 'STATE'}).map { |h| h['state'] }
+  end
+
+  def county_ids_for_state(state_id)
+    @@client.where({fields: @@acs_vars.keys.first, level: 'COUNTY', within: "STATE:#{state_id}"}).map { |h| h['county'] }
+  end
+
+  def tract_ids_for_county_and_state(state_id, county_id)
+    @@clients.where({fields: @@acs_vars.keys.first, level: 'TRACT', within: "STATE:#{state_id}+COUNTY:#{county_id}"}).map { |h| h['tract'] }
+  end
+
   def scrape
-    state_ids = write_state_fields #est 10 minutes
-    # write_cd_fields(state_ids) #est +20 minutes
-    # cds = [] # 52 hashes of X sldl
-    # states.each do |state|
-    #   next if state.nil?
-    #   tot_cds = client.where({ fields: total_fields, level: 'CD', within: "STATE:#{state['state']}"})
-    #   age_cds = client.where({ fields: age_fields, level: 'CD', within: "STATE:#{state['state']}"})
-    #   cds << tot_cds.map{ |h| age_cds.map { |i| h.merge(i) if i['cd']==h['cd'] } }.flatten.compact
+    # state_ids = write_state_fields #est 10 minutes
+    # reset_csv
+    # write_acs_vars
+    # reset_csv
+
+    # state_ids.each_slice(10) do |state_slice|
+    #   next if state_slice.first.to_i < 45
+    #   reset_csv("sldu-from-#{state_slice.first}-to-#{state_slice.last}")
+    #   write_sldu_fields(state_slice) #est +6 minutes
     # end
 
-    # sldus = [] # 52 hashes of X sldu
-    # states.each do |state|
-    #   next if state.nil?
-    #   tot_sldus = client.where({ fields: total_fields, level: 'SLDU', within: "STATE:#{state['state']}"})
-    #   age_sldus = client.where({ fields: age_fields, level: 'SLDU', within: "STATE:#{state['state']}"})
-    #   sldus << tot_sldus.map{ |h| age_sldus.map { |i| h.merge(i) if i['state legislative district (upper chamber)']==h['state legislative district (upper chamber)'] } }.flatten.compact
+    # state_ids.each_slice(10) do |state_slice|
+    #   reset_csv("sldl-from-#{state_slice.first}-to-#{state_slice.last}")
+    #   write_sldl_fields(state_slice) #est +6 minutes
     # end
 
-    # sldls = [] # 52 hashes of X sldl
-    # states.each do |state|
-    #   tot_sldls = client.where({ fields: total_fields, level: 'SLDL', within: "STATE:#{state['state']}"})
-    #   age_sldls = client.where({ fields: age_fields, level: 'SLDL', within: "STATE:#{state['state']}"})
-    #   sldls << tot_sldls.map{ |h| age_sldls.map { |i| h.merge(i) if i['state legislative district (lower chamber)']==h['state legislative district (lower chamber)'] } }.flatten.compact
+    # state_ids.each_slice(10) do |state_slice|
+    #   next if state_slice.first.to_i < 45
+    #   reset_csv("counties-from-#{state_slice.first}-to-#{state_slice.last}")
+    #   write_county_fields(state_slice)
     # end
 
-    # counties = [] # 52 hashes of X counties
-    # states.each do |state|
-    #   next if state.nil?
-    #   tot_counties = client.where({ fields: total_fields, level: 'COUNTY', within: "STATE:#{state['state']}"})
-    #   age_counties = client.where({ fields: age_fields, level: 'COUNTY', within: "STATE:#{state['state']}"})
-    #   counties << tot_counties.map{ |h| age_counties.map { |i| h.merge(i) if i['county']==h['county'] } }.flatten.compact
-    # end
+    state_ids.each do |state_id|
+      next if state_id.to_i < 5
+      reset_csv("tracts-for-state-#{state_id}")
+      write_tract_fields(county_ids_for_state(state_id), state_id)
+    end
+  end
 
-    # tracts = []
-    # counties.each do |county_by_state|
-    #   county_by_state.each do |county|
-    #     tot_tracts = client.where({ fields: total_fields, level: 'TRACT', within: "STATE:#{county['state']}+COUNTY:#{county['county']}"})
-    #     age_tracts = client.where({ fields: age_fields, level: 'TRACT', within: "STATE:#{county['state']}+COUNTY:#{county['county']}"})
-    #     tracts << tot_tracts.map{ |h| age_tracts.map { |i| h.merge(i) if i['tract']==h['tract'] } }.flatten.compact
-    #   end
-    # end
+  def write_acs_vars
+    @csv.close
+    @csv = CSV.open("acs5-vars.csv", "w+", headers: true, force_quotes: true)
+    @csv << %w(field_id label concept predicateType)
 
-    # block_groups = []
-    # tracts.each do |tracts_by_county|
-    #   tracts_by_county.each do |tract|
-    #     tot_block_groups = client.where({ fields: total_fields, level: 'BG', within: "STATE:#{tract['state']}+COUNTY:#{tract['county']}+TRACT:#{tract['tract']}"})
-    #     age_block_groups = client.where({ fields: age_fields, level: 'BG', within: "STATE:#{tract['state']}+COUNTY:#{tract['county']}+TRACT:#{tract['tract']}"})
-    #     block_groups << tot_block_groups.map{ |h| age_block_groups.map { |i| h.merge(i) if i['block group']==h['block group'] } }.flatten.compact
-    #   end
-    # end
-
-      # cds.map do |cds_by_state|
-      #   next if cds_by_state.nil?
-      #   cds_by_state.map do |cd|
-      #     all_fields.map do |f|
-      #       csv << [ cd['state'], cd['cd'], "", "", "", "", "", f, acs_vars[f]['concept']+acs_vars[f]['label'], cd[f] ]
-      #     end
-      #   end
-      # end
-
-      # sldus.map do |sldus_by_state|
-      #   next if sldus_by_state.nil?
-      #   sldus_by_state.map do |sldu|
-      #     all_fields.map do |f|
-      #       csv << [ sldu['state'], "", sldu['sldu'], "", "", "", "", f, acs_vars[f]['concept']+acs_vars[f]['label'], sldu[f] ]
-      #     end
-      #   end
-      # end
-
-      # sldls.map do |sldls_by_state|
-      #   next if sldls_by_state.nil?
-      #   sldls_by_state.map do |sldl|
-      #     all_fields.map do |f|
-      #       csv << [ sldl['state'], "", "", sldl['sldl'], "", "", "", f, acs_vars[f]['concept']+acs_vars[f]['label'], sldl[f] ]
-      #     end
-      #   end
-      # end
-
-      # counties.map do |counties_by_state|
-      #   next if counties_by_state.nil?
-      #   counties_by_state.map do |county|
-      #     all_fields.map do |f|
-      #       csv << [ county['state'], "", "", "", county['county'], "", "", f, acs_vars[f]['concept']+acs_vars[f]['label'], county[f] ]
-      #     end
-      #   end
-      # end
-
-      # tracts.map do |tracts_by_state|
-      #   next if tracts_by_state.nil?
-      #   tracts_by_state.map do |tracts_by_county|
-      #     tracts_by_county.map do |tract|
-      #       all_fields.map do |f|
-      #         csv << [ tract['state'], "", "", "", tract['county'], tract['tract'], "", f, acs_vars[f]['concept']+acs_vars[f]['label'], tract[f] ]
-      #       end
-      #     end
-      #   end
-      # end
-
-      # block_groups.map do |block_groups_by_state|
-      #   next if block_groups_by_state.nil?
-      #   block_groups_by_state.map do |block_groups_by_county|
-      #     block_groups_by_county.map do |block_groups_by_tract|
-      #       block_groups_by_tract.map do |block_group|
-      #         all_fields.map do |f|
-      #           csv << [ block_group['state'], "", "", "", block_group['county'], block_group['tract'], block_group["block group"], f, acs_vars[f]['concept']+acs_vars[f]['label'], block_group[f] ]
-      #         end
-      #       end
-      #     end
-      #   end
-      # end
-
+    @@acs_vars.keys.map do |key|
+      @csv << [ key, @@acs_vars[key]['label'], @@acs_vars[key]['concept'], @@acs_vars[key]['predicateType'] ]
+    end
   end
 
   def write_state_fields
     ids = []
-    state_batch = []
-    @@acs_vars.keys.each_slice(49) do |field_group|
+    @@acs_vars.keys.each_slice(45) do |field_group|
       slice_batch = []
       @@client.where({ fields: field_group.join(','), level: 'STATE' }).map do |state_hash|
         ids << state_hash['state']
-        fg_batch = []
         field_group.map do |f|
-          fg_batch << [ state_hash['state'], "", "", "", "", "", "", f, @@acs_vars[f]['concept']+@@acs_vars[f]['label'], state_hash[f] ]
+          slice_batch << [ build_location(state_hash), state_hash['state'], "", "", "", "", "", "", f, state_hash[f] ]
         end
-        slice_batch << fg_batch.map(&:to_csv)
       end
-      state_batch << slice_batch.flatten
+      slice_batch.map { |row| @csv << row }
     end
-    @csv << state_batch.flatten
     ids.uniq
   end
 
   def write_cd_fields(state_ids)
-    @@acs_vars.keys.each_slice(25) do |field_group|
+    ids = []
+    @@acs_vars.keys.each_slice(45) do |field_group|
+      slice_batch = []
       @@client.where({ fields: field_group.join(','), level: 'CD', within: "STATE:#{state_ids.join(',')}" }).map do |cd_hash|
+        ids << cd_hash['congressional district']
         field_group.map do |f|
-          @csv << [ cd_hash['state'], cd_hash['congressional district'], "", "", "", "", "", f, @@acs_vars[f]['concept']+@@acs_vars[f]['label'], cd_hash[f] ]
+          next unless cd_hash[f]
+          slice_batch << [ build_location(cd_hash), cd_hash['state'], cd_hash['congressional district'], "", "", "", "", "", f, cd_hash[f] ]
         end
       end
+      slice_batch.map { |row| @csv << row }
     end
+    ids.uniq
+  end
+
+  def write_sldu_fields(state_ids)
+    state_ids.map do |state_id|
+      @@acs_vars.keys.each_slice(45) do |field_group|
+        slice_batch = []
+        @@client.where({ fields: field_group.join(','), level: 'SLDU', within: "STATE:#{state_id}" }).map do |sldu_hash|
+          next unless sldu_hash.is_a? Hash
+          field_group.map do |f|
+            next unless sldu_hash[f]
+            slice_batch << [ build_location(sldu_hash), sldu_hash['state'], "", sldu_hash['state legislative district (upper chamber)'], "", "", "", "", f, sldu_hash[f] ]
+          end
+        end
+        slice_batch.map { |row| @csv << row }
+      end
+    end
+  end
+
+  def write_sldl_fields(state_ids)
+    state_ids.map do |state_id|
+      @@acs_vars.keys.each_slice(45) do |field_group|
+        slice_batch = []
+        @@client.where({ fields: field_group.join(','), level: 'SLDL', within: "STATE:#{state_id}" }).map do |sldl_hash|
+          next unless sldl_hash.is_a? Hash
+          field_group.map do |f|
+            next unless sldl_hash[f]
+            slice_batch << [ build_location(sldl_hash), sldl_hash['state'], "", "", sldl_hash['state legislative district (lower chamber)'], "", "", "", f, sldl_hash[f] ]
+          end
+        end
+        slice_batch.map { |row| @csv << row }
+      end
+    end
+  end
+
+  def write_county_fields(state_ids)
+    state_ids.map do |state_id|
+      @@acs_vars.keys.each_slice(45) do |field_group|
+        slice_batch = []
+        @@client.where({ fields: field_group.join(','), level: 'COUNTY', within: "STATE:#{state_id}" }).map do |county_hash|
+          next unless county_hash.is_a? Hash
+          field_group.map do |f|
+            next unless county_hash[f]
+            slice_batch << [ build_location(county_hash), county_hash['state'], "", "", "", county_hash['county'], "", "", f, county_hash[f] ]
+          end
+        end
+        slice_batch.map { |row| @csv << row }
+      end
+    end
+  end
+
+  def write_tract_fields(county_ids, state_id)
+    @@acs_vars.keys.each_slice(45) do |field_group|
+      slice_batch = []
+      @@client.where({ fields: field_group.join(','), level: 'TRACT', within: "STATE:#{state_id}+COUNTY:#{county_ids.join(',')}" }).map do |tract_hash|
+        next unless tract_hash.is_a? Hash
+        field_group.map do |f|
+          next unless tract_hash[f]
+          slice_batch << [ build_location(tract_hash), tract_hash['state'], "", "", "", tract_hash['county'], tract_hash['tract'], "", f, tract_hash[f] ]
+        end
+      end
+      slice_batch.map { |row| @csv << row }
+    end
+  end
+
+  def build_location(hash)
+    ['state', 'congressional district',
+     'state legislative district (upper chamber)',
+     'state legislative district (lower chamber)',
+     'county', 'tract', 'block group'].map do |loc|
+      hash[loc] || 'x'
+    end.join('')
   end
 end
 end
